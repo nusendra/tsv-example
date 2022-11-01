b1869da52e3b62bb6cbd9d20f00a177ae63917d9
Nusendra Hanggarawan <admin@nusendra.com>
2022-11-02 00:41:05 +0700

    feat: init

---
+++ index.ts
@@ -0,0 +1,17 @@
+// @ts-ignore
+import tsv from "typescript-schema-validator";
+import express, { Express, Request, Response } from "express";
+import schema from "./generated-schema";
+
+const app: Express = express();
+const port: number = 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  const a = tsv(schema.UserRequest, { namex: "test" });
+  console.log(a);
+  res.send("Express Server");
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
